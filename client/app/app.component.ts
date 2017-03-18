fc8c23b1fdc06264f59d89ebd903f2bf49db4b46
SelinaDaley <sdmd6@hotmail.com>
2017-03-18 14:18:07 -0400

    initial commit

---
+++ client/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'my-app',
+  templateUrl: 'app/app.template.html',
+  styleUrls: ['app/app.template.css']
+})
+export class AppComponent {
+  name = 'Angular';
+}
